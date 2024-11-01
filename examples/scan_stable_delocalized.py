b6f009578d4b219cf0ea50ed2ea6f80cacf4bdc6
Shivang Rawat <sr6364@nyu.edu>
2024-11-01 19:51:32 -0400

    added more parameters to be input from the batch script

--- examples/scan_stable_delocalized.py
+++ examples/scan_stable_delocalized.py
@@ -21,28 +21,49 @@ cmap = 'viridis'
 
 # Import the arguments
 parser = argparse.ArgumentParser(description="Sparse Matrix Stability Scan")
-parser.add_argument('--MODEL_NAME', type=str, default="scan", help='Model name')
+
+# arguments for the job array code
+parser.add_argument('--TASK_ID', type=int, default=int(os.environ.get('SLURM_ARRAY_TASK_ID', 0)), help="Task ID")
+parser.add_argument('--NUM_TASKS', type=int, default=1, help="Number of tasks")
+
+# parameters of the model 
+parser.add_argument('--MODEL_NAME', type=str, default="delocalized", help='Model name')
 parser.add_argument('--N', type=int, default=100, help="Number of neurons")
 parser.add_argument('--c', type=int, default=100, help="Sparsity")
-parser.add_argument('--num_delta', type=int, default=10, help="Number of delta steps")
-parser.add_argument('--num_gamma', type=int, default=10, help="Number of gamma steps")
-parser.add_argument('--num_trials', type=int, default=10, help="Number of trials")
 parser.add_argument('--mu', type=float, default=0.0, help="Mean of the distribution")
 parser.add_argument('--sigma', type=float, default=0.1, help="Sigma value")
 parser.add_argument('--b0', type=float, default=0.5, help="Input gain for a")
 parser.add_argument('--b1', type=float, default=0.5, help="Input gain for y")
 parser.add_argument('--tauA', type=float, default=0.002, help="Time constant fot a")
 parser.add_argument('--tauY', type=float, default=0.002, help="Time constant for y")
+
+# parameters of simulation
+parser.add_argument('--num_trials', type=int, default=10, help="Number of trials")
+parser.add_argument('--num_delta', type=int, default=10, help="Number of delta steps")
+parser.add_argument('--num_input', type=int, default=10, help="Number of input steps")
+parser.add_argument('--max_input', type=int, default=5, help="Maximum value of the parameter input")
+parser.add_argument('--max_delta', type=int, default=5, help="Maximum value of the parameter delta")
+
+# parse the arguments
 args = parser.parse_args()
 
-# Assign the arguments to the variables
+# argument sof the job array
+task_id = args.TASK_ID
+num_tasks = args.NUM_TASKS
+
+# arguments of the model parameters
 model_name = args.MODEL_NAME
 N = args.N
 c = args.c
 mu = args.mu
+
+# arguments of the simulation
 num_delta = args.num_delta
-num_gamma = args.num_gamma
+num_input = args.num_input
 num_trials = args.num_trials
+max_input = args.max_input
+max_delta = args.max_delta
+
 
 device = torch.device("cpu")
 
@@ -61,6 +82,7 @@ tauY = args.tauY + 0 * torch.abs(torch.randn(N) * 0.001)
 # Wyy = torch.eye(N)
 Way = torch.ones(N, N)
 
+
 def sample_sparse_matrix(N, c, delta, mu):
     mask = torch.bernoulli(torch.full((N, N), c / N)).triu()
     values = torch.normal(mu / c, delta / math.sqrt(c), (N, N))
@@ -70,23 +92,19 @@ def sample_sparse_matrix(N, c, delta, mu):
 
 
 # Define the scan parameters
-delta_range = np.linspace(0, 10, num_delta)
-gamma_range = np.linspace(0.01, 5, num_gamma)
+delta_range = np.linspace(0, max_delta, num_delta)
+input_range = np.linspace(0.01, max_input, num_input)
 
 # define the quantities that we'll calculate
-spectral_radius = torch.zeros((num_delta, num_gamma, num_trials), dtype=torch.float32)
-bool_stable = torch.zeros((num_delta, num_gamma, num_trials), dtype=torch.bool)
+spectral_radius = torch.zeros((num_delta, num_input, num_trials), dtype=torch.float32)
+bool_stable = torch.zeros((num_delta, num_input, num_trials), dtype=torch.bool)
 
-def run_trial(i, j, k, delta, gamma):
+def run_trial(i, j, k, delta, input):
     # Initialize variables for the trial
 
-    # # make input delocalized
+    # make input delocalized
     z = torch.ones(N)
-    z = z / torch.norm(z) * gamma
-
-    # make input one hot
-    # z = torch.zeros(N)
-    # z[0] = gamma
+    z = z / torch.norm(z) * input
 
     Wyy = torch.eye(N) + sample_sparse_matrix(N, c, delta, mu)
 
@@ -111,20 +129,20 @@ def run_trial(i, j, k, delta, gamma):
         return (True, spectral_radius)
 
 results = Parallel(n_jobs=-1, verbose=10)(
-    delayed(run_trial)(i, j, k, delta, gamma)
+    delayed(run_trial)(i, j, k, delta, input)
     for i, delta in enumerate(delta_range)
-    for j, gamma in enumerate(gamma_range)
+    for j, input in enumerate(input_range)
     for k in range(num_trials)
 )
 
 for idx, (i, j, k) in enumerate(
-    (i, j, k) for i in range(num_delta) for j in range(num_gamma) for k in range(num_trials)
+    (i, j, k) for i in range(num_delta) for j in range(num_input) for k in range(num_trials)
 ):
     bool_stable[i, j, k] = results[idx][0]
     spectral_radius[i, j, k] = results[idx][1]
 
 # create a folder in ..data/ to save the results
-folder_name = model_name + '_N_{}_c_{}_mu_{}_num_delta_{}_num_gamma_{}_num_trials_{}'.format(N, c, mu, num_delta, num_gamma, num_trials)
+folder_name = model_name + '_N_{}_c_{}_mu_{}_num_delta_{}_num_input_{}_num_trials_{}_b0_{}_b1_{}'.format(N, c, mu, num_delta, num_input, num_trials, args.b0, args.b1)
 path = os.path.join('..', 'data', folder_name)
 
 if not os.path.exists(path):
@@ -139,7 +157,7 @@ percent_stable = bool_stable.float().mean(dim=2) * 100
 
 # Plot the heatmap
 plt.figure(figsize=(12, 10))
-plt.imshow(percent_stable, extent=[gamma_range.min(), gamma_range.max(), delta_range.min(), delta_range.max()],
+plt.imshow(percent_stable, extent=[input_range.min(), input_range.max(), delta_range.min(), delta_range.max()],
            origin='lower', aspect='auto', cmap='viridis')
 colorbar = plt.colorbar(label="Percent Stable Circuits", fraction=0.046, pad=0.04)
 colorbar.ax.tick_params(labelsize=14)
@@ -168,13 +186,13 @@ plt.savefig(save_fig_path)
 # spectral radius plotting
 spectral_radius_mean = spectral_radius.mean(dim=2)
 plt.figure(figsize=(12, 10))
-plt.imshow(spectral_radius_mean, extent=[gamma_range.min(), gamma_range.max(), delta_range.min(), delta_range.max()],
+plt.imshow(spectral_radius_mean, extent=[input_range.min(), input_range.max(), delta_range.min(), delta_range.max()],
            origin='lower', aspect='auto', cmap=cmap)
 colorbar = plt.colorbar(label="Mean Spectral Radius", fraction=0.046, pad=0.04)
 colorbar.ax.tick_params(labelsize=14)
 
 # Set the labels and title
-plt.xlabel('Gamma', fontsize=22)
+plt.xlabel('Input norm', fontsize=22)
 plt.ylabel(r'$\Delta$', fontsize=22)
 plt.title("Phase Diagram: Mean Spectral Radius", fontsize=20)
 
