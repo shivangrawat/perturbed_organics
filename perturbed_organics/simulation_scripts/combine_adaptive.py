b893cd4643de109119cde2b85e68258afa4f1b9f
Shivang Rawat <60561327+shivangrawat@users.noreply.github.com>
2025-02-23 19:02:34 -0500

    added adaptive code for simulation

---
+++ perturbed_organics/simulation_scripts/combine_adaptive.py
@@ -0,0 +1,94 @@
+import torch
+import os
+import json
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
+cmap = 'plasma'
+
+
+parser = argparse.ArgumentParser(description="Combine the results from the different job arrays")
+parser.add_argument("--folder_loc", type=str, default="", help="Location of the folder")
+parser.add_argument("--folder_name", type=str, default="", help="Name of the folder")
+args = parser.parse_args()
+
+# Define the path and folder name
+folder_loc = args.folder_loc
+folder_name = args.folder_name
+path = os.path.join(folder_loc, folder_name)
+
+# Load parameters from the JSON file
+param_file_path = os.path.join(path, 'parameters.json')
+with open(param_file_path, 'r') as f:
+    params = json.load(f)
+
+# Extract parameters
+N = params['N']
+num_tasks = params['num_tasks']
+num_delta = params['num_delta']
+num_input = params['num_input']
+num_trials = params['num_trials']
+delta_range = np.array(params['delta_range'])
+input_range = np.array(params['input_range'])
+
+# Initialize full results arrays
+condition = torch.full((num_delta, num_input, num_trials), fill_value=-1, dtype=torch.int8)
+spectral_radius = torch.full((num_delta, num_input, num_trials), fill_value=float('nan'), dtype=torch.float16)
+norm_fixed_point_y = torch.full((num_delta, num_input, num_trials, N), fill_value=float("nan"), dtype=torch.float16)
+norm_fixed_point_a = torch.full((num_delta, num_input, num_trials, N), fill_value=float("nan"), dtype=torch.float16)
+actual_fixed_point_y = torch.full((num_delta, num_input, num_trials, N), fill_value=float("nan"), dtype=torch.float16)
+actual_fixed_point_a = torch.full((num_delta, num_input, num_trials, N), fill_value=float("nan"), dtype=torch.float16)
+first_order_perturb_y = torch.full((num_delta, num_input, num_trials, N), fill_value=float("nan"), dtype=torch.float16)
+first_order_perturb_a = torch.full((num_delta, num_input, num_trials, N), fill_value=float("nan"), dtype=torch.float16)
+eigvals_J = torch.full((num_delta, num_input, num_trials, 2 * N), fill_value=float("nan"), dtype=torch.complex64)
+
+print(f"Combining partial results from {num_tasks} tasks")
+
+# Load and combine partial results
+for task_id in range(num_tasks):
+    condition_task = torch.load(os.path.join(path, f'bool_stable_task_{task_id}.pt'))
+    spectral_radius_task = torch.load(os.path.join(path, f'spectral_radius_task_{task_id}.pt'))
+    norm_fixed_point_y_task = torch.load(os.path.join(path, f'norm_fixed_point_y_task_{task_id}.pt'))
+    norm_fixed_point_a_task = torch.load(os.path.join(path, f'norm_fixed_point_a_task_{task_id}.pt'))
+    actual_fixed_point_y_task = torch.load(os.path.join(path, f'actual_fixed_point_y_task_{task_id}.pt'))
+    actual_fixed_point_a_task = torch.load(os.path.join(path, f'actual_fixed_point_a_task_{task_id}.pt'))
+    first_order_perturb_y_task = torch.load(os.path.join(path, f'first_order_perturb_y_task_{task_id}.pt'))
+    first_order_perturb_a_task = torch.load(os.path.join(path, f'first_order_perturb_a_task_{task_id}.pt'))
+    eigvals_J_task = torch.load(os.path.join(path, f'eigvals_J_task_{task_id}.pt'))
+
+    # Find indices where bool_stable_task != -1
+    indices = (condition_task != -1).nonzero(as_tuple=True)
+    condition[indices] = condition_task[indices]
+    spectral_radius[indices] = spectral_radius_task[indices]
+    norm_fixed_point_y[indices] = norm_fixed_point_y_task[indices]
+    norm_fixed_point_a[indices] = norm_fixed_point_a_task[indices]
+    actual_fixed_point_y[indices] = actual_fixed_point_y_task[indices]
+    actual_fixed_point_a[indices] = actual_fixed_point_a_task[indices]
+    first_order_perturb_y[indices] = first_order_perturb_y_task[indices]
+    first_order_perturb_a[indices] = first_order_perturb_a_task[indices]
+    eigvals_J[indices] = eigvals_J_task[indices]
+
+# check if all the files were merged properly
+assert torch.all(condition != -1)
+assert torch.all(~torch.isnan(spectral_radius))
+
+# remove all the files ending with .pt inside the folder
+for file in os.listdir(path):
+    if file.endswith('.pt'):
+        os.remove(os.path.join(path, file))
+
+# Save full results
+torch.save(condition, os.path.join(path, f'condition.pt'))
+torch.save(spectral_radius, os.path.join(path, f'spectral_radius.pt'))
+torch.save(norm_fixed_point_y, os.path.join(path, f'norm_fixed_point_y.pt'))
+torch.save(norm_fixed_point_a, os.path.join(path, f'norm_fixed_point_a.pt'))
+torch.save(actual_fixed_point_y, os.path.join(path, f'actual_fixed_point_y.pt'))
+torch.save(actual_fixed_point_a, os.path.join(path, f'actual_fixed_point_a.pt'))
+torch.save(first_order_perturb_y, os.path.join(path, f'first_order_perturb_y.pt'))
+torch.save(first_order_perturb_a, os.path.join(path, f'first_order_perturb_a.pt'))
+torch.save(eigvals_J, os.path.join(path, f'eigvals_J.pt'))
+
+print("Results saved successfully")
